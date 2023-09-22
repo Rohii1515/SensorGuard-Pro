31626d6f308fbf9617bf52e085cbd12681376f4e
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-09-22 20:10:56 +0530

    setup body

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    
+    name ="sensor",
+    version= "0.0.1",
+    author= "Rohii1515",
+    author_email="rohidasjondhale1515@gmail.com",
+    packages= find_packages()
+)
