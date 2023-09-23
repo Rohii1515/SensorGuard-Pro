8e6ecb50df5e7ba05f09b8c330aedd6971e09647
Rohii1515 <rohidasjondhale1515@gmail.com>
2023-09-23 21:05:18 +0530

    Structure setup done

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import find_packages, setup
+from typing import List
+
+
+# def get_requirements() -> list[str]:
+#     """
+#     This function returns a list of requirements
+#     """
+#     requirements_list: list[str] = []
+#     with open('requirements.txt', 'r') as file:
+#         for line in file:
+#             # Remove leading and trailing whitespace
+#             requirement = line.strip()
+#             if requirement:
+#                 requirements_list.append(requirement)
+
+#     return requirements_list
+
 
 setup(
-    
-    name ="sensor",
-    version= "0.0.1",
-    author= "Rohii1515",
+    name="sensor",
+    version="0.0.1",
+    author="Rohii1515",
     author_email="rohidasjondhale1515@gmail.com",
-    packages= find_packages()
+    packages=find_packages(),
+    install_requires= ["pymongo"]#get_requirements(),
 )
