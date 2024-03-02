fa81b23e95ea5d6dd4b203b889f15ff02094711b
Rohii1515 <rohidasjondhale1515@gmail.com>
2024-03-02 18:57:16 +0530

    sensor data access file

--- sensor/data_access/sensor_data.py
+++ sensor/data_access/sensor_data.py
@@ -15,18 +15,18 @@ class SensorData:
     """
 
     def __init__(self):
-        """
-        """
+
         try:
             self.mongo_client = MongoDBClient(database_name=DATABASE_NAME)
 
         except Exception as e:
             raise SensorException(e, sys)
 
-
-    def save_csv_file(self,file_path ,collection_name: str, database_name: Optional[str] = None):
+    def save_csv_file(
+        self, file_path, collection_name: str, database_name: Optional[str] = None
+    ):
         try:
-            data_frame=pd.read_csv(file_path)
+            data_frame = pd.read_csv(file_path)
             data_frame.reset_index(drop=True, inplace=True)
             records = list(json.loads(data_frame.T.to_json()).values())
             if database_name is None:
@@ -38,14 +38,15 @@ class SensorData:
         except Exception as e:
             raise SensorException(e, sys)
 
-
     def export_collection_as_dataframe(
-        self, collection_name: str, database_name: Optional[str] = None) -> pd.DataFrame:
-        try:
-            """
+        self, collection_name: str, database_name: Optional[str] = None
+    ) -> pd.DataFrame:
+        """
             export entire collectin as dataframe:
             return pd.DataFrame of collection
-            """
+        """
+        try:
+            
             if database_name is None:
                 collection = self.mongo_client.database[collection_name]
             else:
