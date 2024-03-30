128d2c76803e49b7f1d02cb350b9732556777cb9
Rohii1515 <rohidasjondhale1515@gmail.com>
2024-03-31 00:30:25 +0530

    mongodb url connection

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,7 +10,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://shubham:88888@cluster0.ttr7cg5.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url = "mongodb+srv://rohidasjondhaleds:Pass@dscluster.jtkk5r5.mongodb.net/?retryWrites=true&w=majority&appName=DSCluster"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
